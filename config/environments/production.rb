a66bd62ef7dcb1b6ca4c8798b804684dde233b49
rhtprwl <rohit.porwal@ongraph.com>
2015-03-30 11:49:25 +0530

    Assets Precompile True

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,14 +3,14 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.assets.version = '1.0'
   config.log_level = :info
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  config.active_record.dump_schema_after_migration = false
+  config.active_record.dump_schema_after_migration = false  
 end
